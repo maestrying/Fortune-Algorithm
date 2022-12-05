db0a2e124e64abd0cb35b32f54752b74cbe0eece
MaxMaxMaxxx <57087377+maestrying@users.noreply.github.com>
2022-12-05 22:11:40 +1000

    Delete set_point.js

--- set_point.js
+++
@@ -1,8 +0,0 @@
-function Point(x, y) {
-	this.x = x;
-	this.y = y;
-}
-
-Point.prototype.distance = function(a, b) {
-   return(Math.sqrt( (b.x-a.x)*(b.x-a.x) + (b.y-a.y)*(b.y-a.y) )); // пересечение
-}
